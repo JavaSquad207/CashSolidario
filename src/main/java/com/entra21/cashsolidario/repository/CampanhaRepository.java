267923f516f871e7abc86520ef004a67398de031
rafin <rafinhagera@hotmail.com>
2022-10-06 09:56:53 -0300

    Query do Repository

--- src/main/java/com/entra21/cashsolidario/repository/CampanhaRepository.java
+++ src/main/java/com/entra21/cashsolidario/repository/CampanhaRepository.java
@@ -1,11 +1,21 @@
 package com.entra21.cashsolidario.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Service;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
 
 import com.entra21.cashsolidario.entity.Campanha;
 
-@Service
+@Repository
 public interface CampanhaRepository extends JpaRepository<Campanha, Long> {
+	
+	@Query(value = "select c from Campanha c where c.nome = ?1")
+	List<Campanha> findByCampanha(String campanha);
+	
+	@Query(value = "select count(*) from Campanha")
+	Integer quantidadeCampanha();
 
 }
